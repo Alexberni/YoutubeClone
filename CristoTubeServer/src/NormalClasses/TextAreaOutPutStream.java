b0724eaa9ea6150578564bb2d114d01b87c6071b
Alex Benavides De la Cruz <alexbenacruz@gmail.com>
2019-11-03 14:05:57 +0100

    Update TextAreaOutPutStream.java

--- CristoTubeServer/src/NormalClasses/TextAreaOutPutStream.java
+++ CristoTubeServer/src/NormalClasses/TextAreaOutPutStream.java
@@ -1,10 +1,4 @@
 package NormalClasses;
-
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
     
 import java.io.IOException;
 import java.io.OutputStream;
@@ -21,8 +15,6 @@ public final class TextAreaOutPutStream extends OutputStream {
   
       @Override
       public void flush(){ 
-          //textArea.append(sb.toString());
-          //sb.setLength(0);
       }
       
       @Override
@@ -30,19 +22,6 @@ public final class TextAreaOutPutStream extends OutputStream {
   
       @Override
       public void write(int b) throws IOException {
-          /*
-          if (b == '\r')
-              return;
-          
-          if (b == '\n') {
-              textArea.append(sb.toString()+"\n");
-              sb.setLength(0);
-              return;
-          }
-          
-          sb.append((char)b);
-          */
-          //textArea.append(Character.toString((char)b));
           write(new byte[] {(byte) b}, 0, 1);
       }
    
@@ -50,27 +29,7 @@ public final class TextAreaOutPutStream extends OutputStream {
       public void write(byte b[], int off, int len) throws IOException {  
           textArea.append(new String(b, off, len));  
       }    
-      
-      
-      /**
-       * Factory method for creating a PrintStream to print to selected TextArea
-       * @param textArea area where to print
-       * @return created PrintStream ready to print to TextArea
-       * @example
-       * <pre name="test">
-       * #import java.io.*;
-       * #import javax.swing.*;
-       *   JTextArea text = new JTextArea();
-       *   PrintStream tw = TextAreaOutputStream.getTextPrintStream(text);
-       *   tw.print("Hyvää"); // skandit toimi
-       *   tw.print(" ");
-       *   tw.print("päivää!");
-       *   text.getText() === "Hyvää päivää!";
-       *   text.setText("");
-       *   tw.print("ä");
-       *   text.getText() === "ä"; 
-       * </pre>
-       */
+
       public static PrintStream getTextPrintStream(JTextArea textArea) {
               return new PrintStream(new TextAreaOutPutStream(textArea)); //,true,"ISO-8859-1");
       }
