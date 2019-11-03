17d2d8fc9c4c88d9af2966c1fd5329efafa3a5cc
Alex Benavides De la Cruz <alexbenacruz@gmail.com>
2019-11-03 14:04:31 +0100

    Update Usuario.java

--- CristoTubeServer/src/Models/Usuario.java
+++ CristoTubeServer/src/Models/Usuario.java
@@ -1,15 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Models;
 
 import java.util.ArrayList;
 
 /**
  *
- * @author Alex
+ * @author Alex Benavides
  */
 public class Usuario {
 
