9109d3a1244dd0a0b2ff7604865274387a189d71
Alex Benavides De la Cruz <alexbenacruz@gmail.com>
2019-11-03 14:03:39 +0100

    Update MysqlConn.java

--- CristoTubeServer/src/Interfaces/MysqlConn.java
+++ CristoTubeServer/src/Interfaces/MysqlConn.java
@@ -5,22 +5,15 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 /**
  *
- * @author admin
+ * @author Alex Benavides
  */
 public interface MysqlConn {
     
      public static Connection getConn() throws SQLException{
         Connection conn=null;
-       conn=DriverManager.getConnection("jdbc:mysql://52.19.19.65/cristotube", "testpsp" , "@,2,golfoPSP123abcd!");
-        //conn=DriverManager.getConnection("jdbc:mysql://localhost/cristotube", "root" , "");
+       conn=DriverManager.getConnection("address", "root" , "password");
         return conn;
     }
 }
